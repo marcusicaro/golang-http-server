e73d8f7f36f3888ebab19adc2d185a94215f7204
Marcus <marcusicaromc@gmail.com>
2024-11-15 18:56:42 -0300

    implemented handle post request and file creation

--- app/server.go
+++ app/server.go
@@ -20,6 +20,7 @@ func handleRequest(conn net.Conn) {
 	request := ""
 
 	for {
+
 		line, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Println("Failed to get request string.")
@@ -32,13 +33,35 @@ func handleRequest(conn net.Conn) {
 		request += line + "\n"
 	}
 
-	requestLine := strings.Split(request, "\n")
+	requestHeaders := strings.Split(request, "\n")
 
-	requestString := requestLine[0]
+	// fmt.Println(requestHeaders)
+
+	requestString := requestHeaders[0]
 
 	requestParts := strings.Split(requestString, " ")
 
+	method := requestParts[0]
+
 	urlParts := strings.Split(requestParts[1], "/")
+
+	contentLength := 0
+	for _, line := range requestHeaders {
+		if strings.HasPrefix(line, "Content-Length:") {
+			parts := strings.Split(line, " ")
+			contentLength, _ = strconv.Atoi(parts[1])
+			break
+		}
+	}
+
+	body := make([]byte, contentLength)
+	_, err := io.ReadFull(reader, body)
+	if err != nil {
+		fmt.Println("Failed to read request body.")
+		os.Exit(1)
+	}
+
+	fmt.Print(string(body))
 
 	if urlParts[1] == "echo" {
 		echoWord := strings.Replace(urlParts[2], "/", "", -1)
@@ -48,9 +71,10 @@ func handleRequest(conn net.Conn) {
 		userAgent := strings.Split(requestString, ": ")[1]
 		contentLength := strconv.Itoa(len(userAgent))
 		conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + contentLength + "\r\n\r\n" + userAgent))
-	} else if urlParts[1] == "files" {
+	} else if urlParts[1] == "files" && method == "GET" {
+		dir := os.Args[2]
 		fileName := urlParts[2]
-		file, err := os.Open("/tmp/" + fileName)
+		file, err := os.Open(dir + fileName)
 		if err != nil {
 			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 		}
@@ -64,6 +88,16 @@ func handleRequest(conn net.Conn) {
 			contentLength := strconv.Itoa(len(fileContent))
 			conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %s\r\n\r\n%s", contentLength, string(fileContent))))
 		}
+	} else if urlParts[1] == "files" && method == "POST" {
+		dir := os.Args[2]
+		fileName := urlParts[2]
+		err = os.WriteFile(dir+"/"+fileName, body, 0644)
+		if err != nil {
+			conn.Write([]byte("HTTP/1.1 500 Internal Server Error\r\n\r\n"))
+			return
+		}
+		conn.Write([]byte("HTTP/1.1 201 Created\r\nContent-Length: 0\r\n\r\n"))
+
 	} else if urlParts[1] == "/" {
 		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 	} else {
